c747a2e320b5da69e572d3ce189bd7179d4dc4d4
Fabian P. Schmidt <kerel-fs@gmx.de>
2016-01-25 00:54:43 +0100

    mkstatistics: Rename --infile argument to --in

--- mkreceiverjson.py
+++ mkreceiverjson.py
@@ -11,12 +11,6 @@ from wikidotcrawler import fetch_page
 wiki_url = 'http://wiki.glidernet.org/ajax-module-connector.php'
 page_ids = {'list-of-receivers': 22120125}
 
-def process_list(page, timestamp):
-    receivers = parse_receiver_list(page)
-
-    receiverdb = {'receivers': receivers,
-                  'timestamp': timestamp.isoformat()}
-    return receiverdb
 
 def obfuscate_addresses(receiverdb):
     receivers = receiverdb['receivers']
@@ -26,41 +20,34 @@ def obfuscate_addresses(receiverdb):
     receiverdb.update({'receivers': receivers})
     return receiverdb
 
-def save_data(receiverdb, out_file):
-    with open(out_file, 'w') as f:
-        json.dump(receiverdb, f)
-
 
 if __name__ == "__main__":
     PARSER = ArgumentParser(description="""Fetch list-of-receivers from wiki.glidernet.org
                                            and output it into a (machine-readable) file.""")
 
-    PARSER.add_argument("--out","-o",
+    PARSER.add_argument("--out",
                         metavar="OUT_FILE", dest="out_file",
-                        default="receiver-wiki.json",
+                        default="receivers.json",
                         help="Output file. Default:"
-                             "receiver-wiki.json")
-
-
     PARSER.add_argument("--nospam",
                         dest="no_spam",
                         default=False,
                         action="store_true",
-                        help="Obfuscate email addresses (truncate address after '@').")
-
-
+                        help="Obfuscate email addresses (truncate addresses after '@').")
     ARGS = PARSER.parse_args()
 
     print("Fetch {}".format(wiki_url))
     page = fetch_page(wiki_url, page_ids['list-of-receivers'])
     timestamp = datetime.utcnow().replace(microsecond=0)
 
-    print("Parse wiki page.")
-    receiverdb = process_list(page, timestamp)
+    print("Parse list-of-receivers")
+    receiverdb = {'receivers': parse_receiver_list(page),
+                  'timestamp': timestamp.isoformat()}
 
     if ARGS.no_spam:
         print("Obfuscate email addresses.")
         receiverdb = obfuscate_addresses(receiverdb)
 
-    print("Save data to %s." % ARGS.out_file)
-    save_data(receiverdb, ARGS.out_file)
+    print("Save to {}".format(ARGS.out_file))
+    with open(ARGS.out_file, 'w') as f:
+        json.dump(receiverdb, f)
--- mkstatistics.py
+++ mkstatistics.py
@@ -5,8 +5,9 @@ from collections import defaultdict
 from argparse import ArgumentParser
 
 """
-Generate statistics for receiver-wiki.json.
+Generate statistics for receivers.json
 """
+
 
 def print_stats(stations):
     stat_by_country = defaultdict(dict)
@@ -24,13 +25,12 @@ def print_stats(stations):
 if __name__ == "__main__":
     PARSER = ArgumentParser(description="""Generate statistics of a given receiver-wiki.json.""")
 
-    PARSER.add_argument("--infile","-i",
+    PARSER.add_argument("--in",
                         metavar="IN_FILE", dest="in_file",
-                        default="receiver-wiki.json",
+                        default="receivers.json",
                         help="Input file. Default: 'receiver-wiki.json'")
 
     ARGS = PARSER.parse_args()
-
 
     with open(ARGS.in_file) as f:
         receiverdb = json.load(f)
