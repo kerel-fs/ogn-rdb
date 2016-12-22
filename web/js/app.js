65af1ec1824ce4fba947e9380832c3c1d39704a3
pyrog <pyrog@laposte.net>
2016-12-22 14:48:59 +0100

    Add titles to web pages
    Fix issue #25

--- web/js/app.js
+++ web/js/app.js
@@ -7,10 +7,12 @@ ognrdbApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
       when('/receivers', {
+        title: 'OGN Receivers list',
         templateUrl: 'partials/receiver-list.html',
         controller: 'ReceiverListCtrl'
       }).
       when('/receivers/:callsign', {
+        title: ' - OGN Receivers list',
         templateUrl: 'partials/receiver-detail.html',
         controller: 'ReceiverDetailCtrl'
       }).
@@ -18,3 +20,11 @@ ognrdbApp.config(['$routeProvider',
         redirectTo: '/receivers'
       });
   }]);
+
+ognrdbApp.run(['$rootScope', function($rootScope) {
+    $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+        $rootScope.title = current.$$route.title;
+        if(current.params.callsign)
+            $rootScope.title = current.params.callsign + $rootScope.title;
+    });
+}]);
